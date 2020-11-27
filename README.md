16f076e1cd65754b3f258f8fb91d855307c07a6c
voka <chdlswhd7@naver.com>
2020-11-28 01:16:27 +0900

    힘들었다.

--- README.md
+++ README.md
@@ -12,3 +12,49 @@ function handleMenter(){
 
 title.addEventListener("mouseenter",handleMenter);   -> mouseenter이라는 이벤트가 발생할때만 handleMenter이라는 함수가 실행된다.
 title.addEventListener("mouseenter",handleMenter()); -> 새로고침 하자 마자 바로 handleMenter이라는 함수가 실행된다.
+
+
+tailwindcss 설치방법 
+
+
+1.node js 설치
+2.npm init - > modules등등 생김
+3. npm install tailwindcss postcss autoprefixer
+4. postcss.config.js 파일 만들어서 안에 작성
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
+
+npx tailwindcss init 한후 tailwind.config.js에 
+  purge: [
+     './src/**/*.html',
+     './src/**/*.js',
+   ],
+와 같이 넣어 줘도 된다.
+
+소스파일을 빌드시킬 거라면 파일 안에
+@tailwind base;
+@tailwind components;
+@tailwind utilities; 
+를 적는다.
+
+npx tailwindcss-cli@latest build -o (tailwind.css-> css파일 만들고 싶은 경로)
+
+소스파일인
+@tailwind base;
+@tailwind components;
+
+.btn {
+  @apply px-4 py-2 bg-blue-600 text-white rounded;
+}
+
+@tailwind utilities;
+
+와 같이 적용시키고픈 것이 아직 나오지 않았을때 사용하면 좋다.
+
+npx tailwindcss-cli@latest build ./src/tailwind.css -o ./dist/tailwind.css
+
+-o를 기준으로 앞에는 소스파일 경로, 뒤에는 css넣고싶은 경로 
--- chromeapp/JSsrc/clock.js
+++ chromeapp/JSsrc/clock.js
@@ -1,20 +1,31 @@
 const clockContainer = document.querySelector(".js-clock"),
-    clockTitle = clockContainer.querySelector("h1");
+    clockTitle = clockContainer.querySelector(".today"),
+    nTitle = clockContainer.querySelector(".christmas");
 
 
-function getTime(){
+function get(){
     const date = new Date();
-    const christmas = new Date(2020,11,25,00,00,00);
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    console.log(hours - christmas.getHours(),minutes - christmas.getMinutes(),seconds - christmas.getSeconds(), christmas - date);
-    clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    const christmas = new Date(2020,11,25,00,00,00);
+    let mSeconds = christmas.getTime(); 
+    const getday = date.getTime();
+    const nDay = Math.floor((mSeconds - getday)/(1000*60*60*24));
+    mSeconds = mSeconds - nDay*(1000*60*60*24);
+    const nHour = Math.floor((mSeconds - getday)/(1000*60*60));
+    mSeconds = mSeconds - nHour*(1000*60*60);
+    const nMinutes = Math.floor((mSeconds - getday)/(1000*60));
+    mSeconds = mSeconds - nMinutes*(1000*60);
+    const nSeconds = Math.floor((mSeconds - getday)/1000);
+    //console.log(nDay, nHour, nMinutes, nSeconds);
+    clockTitle.innerText = `Current Time :  ${hours < 10 ? `0${hours}` : hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}` : seconds}`;
+    nTitle.innerText = `Time Until Christmas :  ${nDay}D ${nHour < 10 ? `0${nHour}` :  nHour}H ${nMinutes < 10 ? `0${nMinutes}` : nMinutes}M ${nSeconds < 10 ? `0${nSeconds}` : nSeconds}S`;
 }
 
 function init(){
-    getTime();
-    setInterval(getTime,1000);
+    get();
+    setInterval(get,1000);
 }
 
 init();
