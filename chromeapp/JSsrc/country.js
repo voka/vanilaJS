22b7a2890cf9069d4b9e51bfa5911e50ef3d203a
voka <chdlswhd7@naver.com>
2020-11-28 23:18:14 +0900

    google UI완성

--- chromeapp/JSsrc/country.js
+++ chromeapp/JSsrc/country.js
@@ -1,36 +1,36 @@
 const country = document.querySelector(".countryName");
-const saveBtn = document.querySelector(".saveBtn");
+const Msg = document.querySelector(".Msg");
 const refreshBtn = document.querySelector(".refreshBtn");
 const reLocalBtn = document.querySelector(".reLocalBtn");
 
-
-function handleselect(event){
-    const result = event.target.value;
-    if(result == ""){
-        alert("!  Pleses Select Your Contry  !");
-    }
-    else{
-        saveLocal(result);
-    }
-    
+function handleselect(event) {
+  const result = event.target.value;
+  if (result === "") {
+    alert("!  Pleses Select Your Contry  !");
+  } else {
+    saveLocal(result);
+  }
 }
 
-function saveLocal(result){
-    localStorage.setItem('country',result);
+function saveLocal(result) {
+  localStorage.setItem("country", result);
 }
-function refresh(){
-    window.location.reload();
+function refresh() {
+  window.location.reload();
 }
-function refreshLocal(){
-    localStorage.clear();
+function refreshLocal() {
+  localStorage.clear();
 }
-function msg(){
-    const getCountry = localStorage.getItem('country');
-    alert(`You are from a ${getCountry}`);
+function msg() {
+  const getCountry = localStorage.getItem("country");
+  alert(`You are from a ${getCountry}`);
 }
 
-function init(){
-    country.addEventListener("change",handleselect);
+function init() {
+  country.addEventListener("change", handleselect);
+  Msg.addEventListener("click", msg);
+  refreshBtn.addEventListener("click", refresh);
+  reLocalBtn.addEventListener("click", refreshLocal);
 }
 
 init();
